77522bf2f0d00a32e598b10d61d3234fc32a1f1d
Andrej Karavida <andrejkaravida@gmail.com>
2019-09-26 22:03:14 +0200

    End of Section 10

--- DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -8,6 +8,9 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail-resolver';
 import { MemberListResolver } from './_resolvers/member-list-resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit-resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 
 const routes: Routes = [
@@ -15,6 +18,9 @@ const routes: Routes = [
   { path: 'members', component: MemberListComponent, canActivate: [AuthGuard], resolve: {users: MemberListResolver}},
   { path: 'members/:id', component: MemberDetailComponent, canActivate: [AuthGuard], resolve: {user: MemberDetailResolver}},
   { path: 'messages', component: MessagesComponent , canActivate: [AuthGuard]},
+  { path: 'member/edit', component: MemberEditComponent,
+        canActivate: [AuthGuard], resolve: {user: MemberEditResolver},
+        canDeactivate: [PreventUnsavedChanges]},
   { path: 'lists', component: ListsComponent , canActivate: [AuthGuard]},
   { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
